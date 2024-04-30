aa547b9c117ed14c09057cf3f5714f29f632c6fc
Thomas Uriot <thomas.uriot@ncia.nato.int>
2024-04-30 19:53:04 +0200

    add unit test for exceptions throwing + confect a testApp()

---
+++ tests/unit/test_exceptions.py
@@ -0,0 +1,38 @@
+import pytest
+from trading.api.ibapi_class import IBapi
+from trading.core.exceptions.exceptions import PriceNotFloatError, PriceNotLiveError, check_price_is_live_and_is_float
+
+
+@pytest.mark.parametrize(("price", "market_is_live", "reqid", "expected_error"), [
+    (-1, True, 1, PriceNotFloatError),  # raises
+    (-1, False, 1, PriceNotLiveError),  # raises
+    (None, None, 1, PriceNotLiveError),  # raises
+    (None, True, 1, PriceNotFloatError),  # raises
+    (10.0, None, 1, PriceNotLiveError),  # raises
+    (100.0, True, 1, None)  # does not raise
+
+])
+def test_check_price_is_live_and_is_float(app: IBapi,
+                                          price: float | None,
+                                          market_is_live: bool | None,
+                                          reqid: int,
+                                          expected_error: PriceNotLiveError | PriceNotFloatError | None) -> None:
+
+    app.stock_price_dic[reqid].price = price
+    app.stock_price_dic[reqid].market_is_live = market_is_live
+
+    print(price, market_is_live)
+
+    if expected_error is None:
+        print(expected_error)
+        check_price_is_live_and_is_float(app, reqid)
+    elif isinstance(expected_error, PriceNotLiveError):
+        print(expected_error)
+        with pytest.raises(PriceNotLiveError):
+            check_price_is_live_and_is_float(app, reqid)
+    elif isinstance(expected_error, PriceNotFloatError):
+        print(expected_error)
+        with pytest.raises(PriceNotFloatError):
+            check_price_is_live_and_is_float(app, reqid)
+
+    app.disconnect()
